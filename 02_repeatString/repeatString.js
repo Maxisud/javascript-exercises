9791a8a610b27d5b55ea0c696978d25061954eba
maxisud <ilhamzakariya14@gmail.com>
2024-09-16 20:06:27 +0700

    done with challange 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, many) {
+    let result = "";
+    if(many < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < many; i++){
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
