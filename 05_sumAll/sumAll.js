24205642064a415012a200ea62fc9d7b91c9ab49
maxisud <ilhamzakariya14@gmail.com>
2024-09-16 22:24:29 +0700

    done with challenge 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
+    if (typeof firstNum != 'number' || typeof secondNum != 'number' || firstNum < 0 || secondNum < 0 || firstNum % 1 !== 0 || secondNum % 1 !== 0){
+        return "ERROR";
+    } else {
+        if (firstNum < secondNum){
+            for(let i = firstNum; i <= secondNum; i++){
+                result += i;
+            }
+        } else if (firstNum > secondNum){
+            for(let i = firstNum; i >= secondNum; i--){
+                result += i;
+            }
+        }
+    }
+    
+    
 
+    return result;
+
+    
 };
+
+console.log(sumAll(3, -1));
 
 // Do not edit below this line
 module.exports = sumAll;
