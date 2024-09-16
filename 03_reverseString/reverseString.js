a845a39ea91776ed52b36f4aeb6a40b2ba1174c3
maxisud <ilhamzakariya14@gmail.com>
2024-09-16 20:23:47 +0700

    done with challenge 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arrayWord = word.split("");
+    let newWord = [];
+    let n = 0;
+    for(let i = arrayWord.length; i >= 0; i--){
+        newWord[n] = arrayWord[i];
+        n++;
+    }
+    return newWord.join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
