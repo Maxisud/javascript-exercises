20d9a24207f7fa29fe7aee0a17f935e6c58709aa
maxisud <ilhamzakariya14@gmail.com>
2024-09-16 21:53:42 +0700

    done with challenge 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (theArray, ...values) {
+    let counter = 0;
+    for (i = 0; i < theArray.length; i++) {
+        for(j = 0; j < values.length; j++) {
+            if (theArray[i] === values[j]) {
+                theArray.splice(i, 1);
+                i = 0;
+            }
+        };
+    }
+
+    return theArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 4, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
